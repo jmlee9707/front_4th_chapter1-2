4222023833a9445d052b9dbdf147e36eb6153bfa
jmlee9707 <jmlee9707@naver.com>
2024-12-26 21:51:04 +0900

    ✨ feat: normalizeVNode, createElement 함수 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,32 @@
-import { addEvent } from "./eventManager";
+import { updateAttributes } from "./updateAttributes";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // 1. null, undefined, boolean 면 빈 텍스트 노드를 반환
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return document.createTextNode("");
 
-function updateAttributes($el, props) {}
+  // 2. vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환
+  if (typeof vNode === "number" || typeof vNode === "string")
+    return document.createTextNode(String(vNode));
+
+  // 3.배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    const nodeList = vNode.map((child) => createElement(child));
+    nodeList.forEach((node) => fragment.appendChild(node));
+
+    return fragment;
+  }
+
+  // 4. 위 경우가 아니면 실제 DOM 요소를 생성
+  const { type, props, children = [] } = vNode;
+  const $node = document.createElement(type);
+
+  updateAttributes($node, null, props ?? {});
+
+  children
+    .filter((child) => !!child)
+    .forEach((child) => $node.appendChild(createElement(child)));
+
+  return $node;
+}
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,23 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  // 1. null, undefined 또는 boolean 타입일 경우 빈 문자열을 반환
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return "";
+
+  // 2. 문자열 또는 숫자일 경우 문자열로 변환하여 반환
+  if (typeof vNode === "number" || typeof vNode === "string")
+    return String(vNode);
+
+  // 3. 함수일 경우 해당 함수를 호출하여 반환된 결과를 재귀적으로 표준화  : 함수형 컴포넌트
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({ ...vNode.props, children: vNode.children }),
+    );
+  }
+
+  // 4. 그 외의 경우, vNode의 자식 요소들을 재귀적으로 표준화하고, falsy 값 필터링
+  const refactorNode = (vNode.children || [])
+    .map((child) => normalizeVNode(child))
+    .filter((child) => !!child);
+
+  return { ...vNode, children: refactorNode };
 }
