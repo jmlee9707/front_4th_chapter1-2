18e9c810d4da1bffa2436027219baad8e003125f
jmlee9707 <jmlee9707@naver.com>
2024-12-27 01:59:55 +0900

    ✨ feat : render element code 구현

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,17 +1,20 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-export function renderElement(vNode, container) {
-  // vNode 정규화 한다음
-  const refactNode = normalizeVNode(vNode);
+import { updateElement } from "./updateElement";
 
+const dom = new WeakMap();
+
+export function renderElement(vNode, container) {
+  const refactNode = normalizeVNode(vNode);
   if (!container.hasChildNodes()) {
     const $root = createElement(refactNode);
     container.appendChild($root);
+  } else {
+    const oldNode = dom.get("container");
+    updateElement(container, refactNode, oldNode);
   }
 
   setupEventListeners(container);
-
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  dom.set(container, refactNode);
 }
